1d04ec2d911888032e626e69dfec70aa0db93c16
Jaga <Jaga@DESKTOP-UHCTFMH>
2020-09-16 19:15:14 -0400

    remove some old commented out code

--- Combat.cpp
+++ Combat.cpp
@@ -122,75 +122,11 @@ namespace olc
             return false;
         }
     }
-   /* struct matrix3x3
-    {
-        float m[3][3];
-    };
-
-    void Identity(matrix3x3& mat) {
-
-        mat.m[0][0] = 1.0f; mat.m[1][0] = 0.0f; mat.m[2][0] = 0.0f;
-        mat.m[0][1] = 0.0f; mat.m[1][1] = 1.0f; mat.m[2][1] = 0.0f;
-        mat.m[0][2] = 0.0f; mat.m[1][2] = 0.0f; mat.m[2][2] = 1.0f;
-    }
-
-
-    void Translate(matrix3x3& mat, float ox, float oy) {
-        mat.m[0][0] = 1.0f; mat.m[1][0] = 0.0f; mat.m[2][0] = ox;
-        mat.m[0][1] = 0.0f; mat.m[1][1] = 1.0f; mat.m[2][1] = oy;
-        mat.m[0][2] = 0.0f; mat.m[1][2] = 0.0f; mat.m[2][2] = 1.0f;
-    }
-
-    void Rotate(matrix3x3& mat, float fTheta) {
-        mat.m[0][0] = cosf(fTheta);  mat.m[1][0] = sinf(fTheta); mat.m[2][0] = 0.0f;
-        mat.m[0][1] = -sinf(fTheta); mat.m[1][1] = cosf(fTheta); mat.m[2][1] = 0.0f;
-        mat.m[0][2] = 0.0f;          mat.m[1][2] = 0.0f;         mat.m[2][2] = 1.0f;
-
-    }
-
-
-    void MatixMultiply(matrix3x3& matResult, matrix3x3& matA, matrix3x3& matB) {
-
-        for (int c = 0; c < 3; c++) {
-            for (int r = 0; r < 3; r++) {
-                matResult.m[c][r] = matA.m[0][r] * matB.m[c][0] +
-                    matA.m[1][r] * matB.m[c][1] +
-                    matA.m[2][r] * matB.m[c][2];
-            }
-        }
-    }
-
-    void Forward(matrix3x3& mat, float in_x, float in_y, float& out_x, float& out_y) {
-        out_x = in_x * mat.m[0][0] + in_y * mat.m[1][0] + mat.m[2][0];
-        out_y = in_x * mat.m[0][1] + in_y * mat.m[1][1] + mat.m[2][1];
-
-
-    }
-
-    void Invert(matrix3x3& matIn, matrix3x3& matOut)
-    {
-        float det = matIn.m[0][0] * (matIn.m[1][1] * matIn.m[2][2] - matIn.m[1][2] * matIn.m[2][1]) -
-            matIn.m[1][0] * (matIn.m[0][1] * matIn.m[2][2] - matIn.m[2][1] * matIn.m[0][2]) +
-            matIn.m[2][0] * (matIn.m[0][1] * matIn.m[1][2] - matIn.m[1][1] * matIn.m[0][2]);
-
-        float idet = 1.0f / det;
-        matOut.m[0][0] = (matIn.m[1][1] * matIn.m[2][2] - matIn.m[1][2] * matIn.m[2][1]) * idet;
-        matOut.m[1][0] = (matIn.m[2][0] * matIn.m[1][2] - matIn.m[1][0] * matIn.m[2][2]) * idet;
-        matOut.m[2][0] = (matIn.m[1][0] * matIn.m[2][1] - matIn.m[2][0] * matIn.m[1][1]) * idet;
-        matOut.m[0][1] = (matIn.m[2][1] * matIn.m[0][2] - matIn.m[0][1] * matIn.m[2][2]) * idet;
-        matOut.m[1][1] = (matIn.m[0][0] * matIn.m[2][2] - matIn.m[2][0] * matIn.m[0][2]) * idet;
-        matOut.m[2][1] = (matIn.m[0][1] * matIn.m[2][0] - matIn.m[0][0] * matIn.m[2][1]) * idet;
-        matOut.m[0][2] = (matIn.m[0][1] * matIn.m[1][2] - matIn.m[0][2] * matIn.m[1][1]) * idet;
-        matOut.m[1][2] = (matIn.m[0][2] * matIn.m[1][0] - matIn.m[0][0] * matIn.m[1][2]) * idet;
-        matOut.m[2][2] = (matIn.m[0][0] * matIn.m[1][1] - matIn.m[0][1] * matIn.m[1][0]) * idet;
-    }*/
-
 }
 
 class Combat : public olc::PixelGameEngine
 {
     std::wstring sBoard;
-   /* std::wstring sTank;*/
    
 
     class Tank {
@@ -212,7 +148,7 @@ class Combat : public olc::PixelGameEngine
    };
        
 
-    Tank myTank, otherTank, prevTank, prevOtherTank;
+    Tank myTank, otherTank;
     int nBoardWidth;
     int nBoardHeight;
     int nSquareSize;
@@ -337,8 +273,8 @@ class Combat : public olc::PixelGameEngine
 
         DrawDecal({0, 0}, decBG);
 
-        prevTank = myTank;
-        prevOtherTank = otherTank;
+       /* prevTank = myTank;
+        prevOtherTank = otherTank;*/
 
         // Handle Input
         // Handle Player Input
@@ -461,7 +397,7 @@ class Combat : public olc::PixelGameEngine
         for (int k = 0; k < 2; k++) {
             olc::Pixel p, curp;
             Tank* curTank = nullptr;
-            Tank lastTank = prevTank;
+           /* Tank lastTank = prevTank;*/
             Tank* curoppTank = nullptr;
 
             if (k == 0) {
@@ -470,7 +406,7 @@ class Combat : public olc::PixelGameEngine
                  curomatFinalInv = mat2FinalInv;*/
                 curp = olc::Pixel(255, 0, 0);
                 curTank = &myTank;
-                lastTank = prevTank;
+             /*   lastTank = prevTank;*/
                 curoppTank = &otherTank;
 
             }
@@ -481,7 +417,7 @@ class Combat : public olc::PixelGameEngine
                 curp = olc::Pixel(0, 0, 255);
                 curTank = &otherTank;
                 curoppTank = &myTank;
-                lastTank = prevOtherTank;
+               /* lastTank = prevOtherTank;*/
             }
 
             // Work out bounding box of sprite post-transformation
